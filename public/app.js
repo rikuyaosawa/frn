c8c3986010bb06a28949215dfe978f9473b90f6a
rikuyaosawa <rikuyaosawa@gmail.com>
2024-11-11 21:23:31 +0900

    on auth state changed

--- public/app.js
+++ public/app.js
@@ -13,3 +13,15 @@ const provider = new firebase.auth.GoogleAuthProvider();
 signInBtn.onclick = () => auth.signInWithPopup(provider);
 
 signOutBtn.onclick = () => auth.signOut();
+
+auth.onAuthStateChanged(function (user) {
+    if (user) {
+        whenSignedIn.hidden = false;
+        whenSignedOut.hidden = true;
+        userDetails.innerHTML = `<h3>Hello ${user.displayName}!</h3><p>${user.uid}</p>`;
+    } else {
+        whenSignedIn.hidden = true;
+        whenSignedOut.hidden = false;
+        userDetails.innerHTML = "";
+    }
+});
